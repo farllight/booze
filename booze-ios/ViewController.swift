5b706c7ed57dae13daeb06467b2a6eed9d28f3a7
Владислав Марков <v.markov@kingbird.ru>
2018-10-19 21:38:02 +0400

    Create project

---
+++ booze-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  booze-ios
+//
+//  Created by Владислав Марков on 19/10/2018.
+//  Copyright © 2018 Владислав Марков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
