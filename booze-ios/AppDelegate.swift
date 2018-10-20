66da5f5d6e66f7c5566a6f13540d1cd8d3d4b425
Владислав Марков <v.markov@kingbird.ru>
2018-10-20 04:30:44 +0400

    Change fonts

--- booze-ios/AppDelegate.swift
+++ booze-ios/AppDelegate.swift
@@ -14,9 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window?.makeKeyAndVisible()
-        window?.rootViewController = CodeConformitionVC.storyboardInstance()
+        window?.rootViewController = MainTabBarController()
                 
-        return true
+        return true 
     }
 }
 
